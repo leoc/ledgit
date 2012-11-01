c0f1d637df482648ecf8e0a9f450bd97faf36291
Arthur Andersen <leoc.git@gmail.com>
2012-11-01 14:21:24 +0100

    Fix handler for new conventions

--- lib/handler.rb
+++ lib/handler.rb
@@ -1,34 +1,25 @@
 class Ledgit
   module Handler
-
     class << self
 
+      ##
+      # This list contains the handler modules for each supported
+      # banking website.
       def list
         @list ||= {}
       end
 
-      def get_class key
-        klass = list[key]
-        raise "no such handler for '#{key}'" unless klass
-        klass
+      ##
+      # Retrieve an array of handlers for a specific account handler.
+      def modules_for key
+        mod = list[key]
+        raise "no such handler for '#{key}'" unless mod
+        mod = [ mod ] unless mod.is_a?(Array)
+        mod
       end
 
     end
-
-    module ClassMethods
-      def prop
-        @properties
-      end
-
-      def get_data from_date = nil, to_date = nil
-        puts "** Logging in username: #{prop[:username]}"
-        login(prop[:username], prop[:password])
-        puts "** Downloading transaction for timespan: #{from_date.strftime('%Y/%m/%d')} to #{to_date.strftime('%Y/%m/%d')}"
-        data = download_data(from_date, to_date)
-        puts "** Parsing data"
-        parse_data(data)
-      end
-    end
-
   end
 end
+
+Dir["#{File.dirname(__FILE__)}/handler/**/*.rb"].each { |file| require(file) }
