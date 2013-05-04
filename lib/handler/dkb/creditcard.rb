b5ed2185e0e9b7b818e02d76ca769381ec1c2a39
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-05-04 19:29:08 +0200

    Fix dkb handler for new website layout

--- lib/handler/dkb/creditcard.rb
+++ lib/handler/dkb/creditcard.rb
@@ -6,16 +6,16 @@ class Ledgit
   module Handler
     module DKB
       module CreditCard
-        def login username, password
+        def login(username, password)
           # log into the online banking website
-          @agent.get "https://banking.dkb.de:443/dkb/-?$javascript=disabled"
+          @agent.get 'https://banking.dkb.de:443/dkb/-?$javascript=disabled'
           form = @agent.page.forms.first
           form.j_username = username
           form.j_password = password
           form.submit
 
           # go to the transaction listing for the correct account type
-          @agent.page.link_with(text: "Umsätze").click
+          @agent.page.link_with(text: /Finanzstatus/).click
           @agent.page.link_with(text: /Kreditkartenumsätze/).click
         end
 
@@ -24,37 +24,36 @@ class Ledgit
         # for the given timespan (fromTransactionDate,
         # toTransactionDate)
         def download_data
-          form = @agent.page.forms[1]
+          form = @agent.page.forms[2]
 
-          postingDate = (last_update_at - 35).strftime('%d.%m.%Y')
-          toPostingDate = Date.today.strftime('%d.%m.%Y')
-
+          posting_date = (last_update_at - 35).strftime('%d.%m.%Y')
+          to_posting_date = Date.today.strftime('%d.%m.%Y')
 
           form.field_with(name: /slCreditCard/).
             option_with(text: /#{Regexp.escape(cardnumber)}/).select
-          form.radiobutton_with(name: /searchPeriod/, value: "0").check
-          form.field_with(name: "postingDate").value = postingDate
-          form.field_with(name: "toPostingDate").value = toPostingDate
+          form.radiobutton_with(name: /searchPeriod/, value: '0').check
+          form.field_with(name: 'postingDate').value = posting_date
+          form.field_with(name: 'toPostingDate').value = to_posting_date
           form.submit
 
-          @agent.page.link_with(text: "CSV-Export").click
+          @agent.page.link_with(href: /csvExport/).click
           @agent.page.body
         end
 
         ##
         # parses the raw data from the DKB website into a hash that
         # ledge_it can work with.
-        def parse_data data
-          data.encode! "UTF-8", "ISO-8859-1"
-          data.gsub!(/\A.*\n\n.*\n\n/m, "")
+        def parse_data(data)
+          data.encode!('UTF-8', 'ISO-8859-1')
+          data.gsub!(/\A.*\n\n.*\n\n/m, '')
 
           result = CSV.parse(data, col_sep: ';', headers: :first_row)
           result.map do |row|
             {
-              booking_date: Date.parse(row["Belegdatum"]),
-              payment_date:  Date.parse(row["Wertstellung"]),
-              description:  row["Umsatzbeschreibung"],
-              amount: row["Betrag (EUR)"].gsub('.','').gsub(',','.').to_f
+              booking_date: Date.parse(row['Belegdatum']),
+              payment_date:  Date.parse(row['Wertstellung']),
+              description:  row['Umsatzbeschreibung'],
+              amount: row['Betrag (EUR)'].gsub('.', '').gsub(',', '.').to_f
             }
           end
         end
@@ -63,7 +62,7 @@ class Ledgit
   end
 end
 
-Ledgit::Handler.list["dkb/creditcard"] = [
+Ledgit::Handler.list['dkb/creditcard'] = [
                                           Ledgit::Handler::CreditCard,
                                           Ledgit::Handler::DKB::CreditCard
                                          ]
--- lib/handler/dkb/giro.rb
+++ lib/handler/dkb/giro.rb
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-require "csv"
-require "handler/giro"
+require 'csv'
+require 'handler/giro'
 
 class Ledgit
   module Handler
     module DKB
       module Giro
-        def login username, password
+        def login(username, password)
           # log into the online banking website
-          @agent.get "https://banking.dkb.de:443/dkb/-?$javascript=disabled"
+          @agent.get 'https://banking.dkb.de:443/dkb/-?$javascript=disabled'
+
           form = @agent.page.forms.first
           form.j_username = username
           form.j_password = password
           form.submit
 
           # go to the transaction listing for the correct account type
-          @agent.page.link_with(text: "Umsätze").click
+          @agent.page.link_with(text: /Finanzstatus/).click
           @agent.page.link_with(text: /Kontoumsätze/).click
         end
 
@@ -24,40 +25,40 @@ class Ledgit
         # for the given timespan (fromTransactionDate,
         # toTransactionDate)
         def download_data
-          form = @agent.page.forms[1]
+          form = @agent.page.forms[2]
 
-          transactionDate = (last_update_at - 3).strftime('%d.%m.%Y')
-          toTransactionDate = Date.today.strftime('%d.%m.%Y')
+          transaction_date = (last_update_at - 3).strftime('%d.%m.%Y')
+          to_transaction_date = Date.today.strftime('%d.%m.%Y')
 
-          form.field_with(:name => 'slBankAccount').
+          form.field_with(name: 'slBankAccount').
             option_with(text: /#{Regexp.escape(cardnumber)}/).select
-          form.radiobutton_with(name: /searchPeriodRadio/, value: "1").check
-          form.field_with(name: "transactionDate").value = transactionDate
-          form.field_with(name: "toTransactionDate").value = toTransactionDate
+          form.radiobutton_with(name: /searchPeriodRadio/, value: '1').check
+          form.field_with(name: 'transactionDate').value = transaction_date
+          form.field_with(name: 'toTransactionDate').value = to_transaction_date
           form.submit
 
-          @agent.page.link_with(text: "CSV-Export").click
+          @agent.page.link_with(href: /csvExport/).click
           @agent.page.body
         end
 
         ##
         # parses the raw data from the DKB website into a hash that
         # ledge_it can work with.
-        def parse_data data
-          data.encode! "UTF-8", "ISO-8859-1"
-          data.gsub!(/\A.*\n\n.*\n\n/m, "")
+        def parse_data(data)
+          data.encode! 'UTF-8', 'ISO-8859-1'
+          data.gsub!(/\A.*\n\n.*\n\n/m, '')
 
           result = CSV.parse(data, col_sep: ';', headers: :first_row)
           result.map do |row|
             {
-              booking_date: Date.parse(row["Buchungstag"]),
-              payment_date:  Date.parse(row["Wertstellung"]),
-              partner:  row["Auftraggeber/Begünstigter"],
-              text:  row["Buchungstext"],
-              description:  row["Verwendungszweck"],
-              account_number:  row["Kontonummer"],
-              bank_code: row["BLZ"],
-              amount: row["Betrag (EUR)"].gsub('.','').gsub(',','.').to_f
+              booking_date: Date.parse(row['Buchungstag']),
+              payment_date:  Date.parse(row['Wertstellung']),
+              partner:  row['Auftraggeber / Begünstigter'],
+              text:  row['Buchungstext'],
+              description:  row['Verwendungszweck'],
+              account_number:  row['Kontonummer'],
+              bank_code: row['BLZ'],
+              amount: row['Betrag (EUR)'].gsub('.', '').gsub(',', '.').to_f
             }
           end
         end
@@ -66,7 +67,7 @@ class Ledgit
   end
 end
 
-Ledgit::Handler.list["dkb/giro"] = [
+Ledgit::Handler.list['dkb/giro'] = [
                                     Ledgit::Handler::DKB::Giro,
                                     Ledgit::Handler::Giro
                                    ]
