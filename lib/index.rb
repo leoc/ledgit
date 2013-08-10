d821bc6067ad4bce67bcf01e326702e21d495fb0
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-08-10 15:29:30 +0200

    Clean up index class

--- lib/index.rb
+++ lib/index.rb
@@ -1,7 +1,8 @@
 class Ledgit
   class Index < Hash
 
-    def load accounts
+    # TODO: refactor ... method too long
+    def load(accounts)
       accounts.each do |account|
         if account.load_for_index?
           transaction = nil
@@ -26,8 +27,8 @@ class Ledgit
                 transaction[:account_number] = $1
               elsif line =~ /^[ ]+; transaction_bank_code: (.*)$/
                 transaction[:bank_code] = $1
-              elsif line =~ /^\d+\/\d+\/\d+(=\d+\/\d+\/\d+)?( . | )(.*)$/
-                self.add transaction if transaction
+              elsif line =~ %r(^\d+/\d+/\d+(=\d+/\d+/\d+)?( . | )(.*)$)
+                add(transaction) if transaction
                 transaction = {}
                 transaction[:name] = $3
               end
@@ -37,56 +38,57 @@ class Ledgit
       end
     end
 
-    def add transaction
+    # TODO: refactor ... method too long
+    def add(transaction)
       entry = (self["#{transaction[:partner]}-#{transaction[:account_number]}-#{transaction[:bank_code]}"] ||= {})
 
       entry[:names] ||= {}
       entry[:names][transaction[:name]] =
-        (entry[:names][transaction[:name]] or 0) + 1
+        (entry[:names][transaction[:name]] || 0) + 1
 
-      (transaction[:add_accounts] or {}).each_pair do |account, count|
+      (transaction[:add_accounts] || {}).each_pair do |account, count|
         entry[:add_accounts] ||= {}
         entry[:add_accounts][account] =
-          (entry[:add_accounts][account] or 0) + count
+          (entry[:add_accounts][account] || 0) + count
       end
 
-      (transaction[:sub_accounts] or {}).each_pair do |account, count|
+      (transaction[:sub_accounts] || {}).each_pair do |account, count|
         entry[:sub_accounts] ||= {}
         entry[:sub_accounts][account] =
-          (entry[:sub_accounts][account] or 0) + count
+          (entry[:sub_accounts][account] || 0) + count
       end
     end
 
-    def get_partner_name partner, account_number, bank_code
+    def get_partner_name(partner, account_number, bank_code)
       entry = self["#{partner}-#{account_number}-#{bank_code}"]
       current_name = nil
       if entry
         highest = 0
-        (entry[:names] or {}).each_pair do |name, count|
+        (entry[:names] || {}).each_pair do |name, count|
           current_name = name if count > highest
         end
       end
       current_name
     end
 
-    def get_subtraction_account partner, account_number, bank_code
+    def get_subtraction_account(partner, account_number, bank_code)
       entry = self["#{partner}-#{account_number}-#{bank_code}"]
       current_account = nil
       if entry
         highest = 0
-        (entry[:sub_accounts] or {}).each_pair do |account, count|
+        (entry[:sub_accounts] || {}).each_pair do |account, count|
           current_account = account if count > highest
         end
       end
       current_account
     end
 
-    def get_addition_account partner, account_number, bank_code
+    def get_addition_account(partner, account_number, bank_code)
       entry = self["#{partner}-#{account_number}-#{bank_code}"]
       current_account = nil
       if entry
         highest = 0
-        (entry[:add_accounts] or {}).each_pair do |account, count|
+        (entry[:add_accounts] || {}).each_pair do |account, count|
           current_account = account if count > highest
         end
       end
