9afbe5aa14b8cbc08ad313a8fa389bfb9cc28917
Arthur Andersen <arthur+github@andersen.berlin>
2017-10-06 10:57:41 +0200

    Refactor `Ledgit::Account`

--- lib/account.rb
+++ lib/account.rb
@@ -1,47 +1,25 @@
 class Ledgit
   class Account < Hash
+    attr_reader(
+      :name,
+      :ledger_file,
+      :handler,
+      :filters,
+      :credentials
+    )
 
-    attr_reader :name, :cardnumber, :username, :password
-    attr_reader :ledger_file, :index, :filters
-
-    def initialize(hash, index)
-      @index = index
-      @name = hash['name']
-      @cardnumber = hash['cardnumber']
-      @username = hash['username']
-      @password = hash['password']
-      @ledger_file = File.expand_path(hash['ledger_file'])
-      @filters = hash['filters']
-
-      Ledgit::Handler.modules_for(hash['handler']).each do |mod|
-        extend mod
-      end
-    end
-
-    ##
-    # Reads the last update date from the ledger file
-    def last_update_at
-      File.open(ledger_file, 'r') do |file|
-        first_line = file.gets
-        if first_line && first_line =~ /^; Last Update: ([\d\/]+)$/
-          @last_update_at = Date.parse($1)
-        end
-      end
-      @last_update_at ||= Date.today
-    end
-
-    ##
-    # Writes the last update into the first line of the ledger file.
-    def set_last_update!
-      File.open(ledger_file, 'r+') do |file|
-        file.puts "; Last Update: #{Date.today.strftime("%Y/%m/%d")}"
-      end
+    def initialize(config)
+      @name = config['name']
+      @ledger_file = config['ledger_file']
+      @filters = config['filters']
+      @handler = config['handler']
+      @credentials = config['credentials']
     end
 
     ##
     # Checks whether the transaction should be filtered or not.
     def filter?(transaction)
-      (filters or []).each do |filter|
+      (filters || []).each do |filter|
         match = true
         filter.each do |key, value|
           match = false if transaction[key.to_sym] !~ /#{value}/
