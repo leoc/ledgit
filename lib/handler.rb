b768a5af801830a7705db33e94fe80b0a3b263e2
Arthur Andersen <arthur+github@andersen.berlin>
2017-10-06 10:59:55 +0200

    Refactor `Ledgit::Handler` for inheritance

--- lib/handler.rb
+++ lib/handler.rb
@@ -1,23 +1,57 @@
 class Ledgit
-  module Handler
-    class << self
+  class Handler
+    attr_reader(:account, :file)
 
-      ##
-      # This list contains the handler modules for each supported
-      # banking website.
-      def list
-        @list ||= {}
+    def initialize(account)
+      @account = account
+      @file = Ledgit::LedgerFile.new(account.ledger_file)
+    end
+
+    def get_transactions
+      raise('Running blank handler. Use bank specific handlers.')
+    end
+
+    def run!
+      puts "Running handler for #{account} ..."
+      file = LedgerFile.new(account.ledger_file)
+
+      puts "Retrieving transactions ..."
+      transactions = get_transactions
+
+      puts "Appending transactions to file ..."
+      transactions.each do |transaction|
+        file.append_transaction(transaction)
       end
 
-      ##
-      # Retrieve an array of handlers for a specific account handler.
-      def modules_for(key)
-        mod = list[key]
-        raise "no such handler for '#{key}'" unless mod
-        mod = [mod] unless mod.is_a?(Array)
-        mod
-      end
+      puts "Finishing up ..."
+      file.set_last_update!
+    end
 
+    def self.list
+      @list ||= {}
+    end
+
+    def self.[](key)
+      list[key] || raise("no such handler for '#{key}'")
+    end
+
+    def self.inherited(handler_class)
+      return if handler_class.to_s !~ /Ledgit::Handler::/
+      identifier =
+        handler_class
+          .to_s
+          .gsub('Ledgit::Handler::', '')
+          .gsub('::', '/')
+          .gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+          .tr('-', '_')
+          .downcase
+      Handler.list[identifier] = handler_class
+    end
+
+    def self.get(account)
+      handler_class = Handler[account.handler]
+      handler_class.new(account)
     end
   end
 end
