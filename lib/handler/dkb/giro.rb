1cb19334ee66b54bbba81a23b00ffe15452ee231
Arthur Leonard Andersen <arthur+github@andersen.berlin>
2017-12-30 22:17:16 +0100

    Add classify call to DKB/Giro handler

--- lib/handler/dkb/giro.rb
+++ lib/handler/dkb/giro.rb
@@ -61,7 +61,10 @@ class Ledgit
 
         def receiving_account(transaction)
           if transaction['Betrag (EUR)'][0] == '-'
-            'Expenses:Unknown'
+            @classifier.classify(
+              tags: transaction_tags(transaction),
+              transfer: :out
+            ) || 'Expenses:Unknown'
           else
             account.name
           end
@@ -71,7 +74,10 @@ class Ledgit
           if transaction['Betrag (EUR)'][0] == '-'
             account.name
           else
-            'Income:Unknown'
+            @classifier.classify(
+              tags: transaction_tags(transaction),
+              transfer: :in
+            ) || 'Income:Unknown'
           end
         end
 
